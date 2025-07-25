ec20fcc51566596d0b81a0877e903d9c8ae3f05c
robert thornton <robert.thornton92@gmail.com>
2025-07-25 14:21:27 +0300

    Feat: added delete function for recipes

--- routers/recipeRouter.js
+++ routers/recipeRouter.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 const recipeController = require('../controllers/recipeController');
+const authController = require('../controllers/authController');
 const recipeRouter = Router();
 
-recipeRouter.get('/new', recipeController.createRecipe);
+recipeRouter.get('/new', authController.ensureAuthenticated, recipeController.createRecipe);
 recipeRouter.post('/add', recipeController.addRecipe);
 
 // delete
+recipeRouter.post('/:id/delete', recipeController.deleteRecipe);
 // edit
+// recipeRouter.get('/:id/edit', recipeController.editRecipe);
+// recipeRouter.post('/:id/edit', recipeController.updateRecipe);
 // get recipe
 
 module.exports = recipeRouter;
