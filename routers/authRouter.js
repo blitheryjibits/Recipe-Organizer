64ba015cc8be33d5920b39c6400ed81c3cb51e39
robert thornton <robert.thornton92@gmail.com>
2025-07-25 14:20:02 +0300

    Feat: added paths for signout and user profile view

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -8,4 +8,8 @@ authRouter.post('/signup', authController.createAccount);
 authRouter.get('/signin', authController.signin);
 authRouter.post('/signin', authController.verifyAccount);
 
+authRouter.post('/signout', authController.signout);
+
+authRouter.get('/profile', authController.ensureAuthenticated, authController.viewProfile);
+
 module.exports = authRouter;
